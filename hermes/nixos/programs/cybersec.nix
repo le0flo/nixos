47560dc733fc4c72e61a558b3a288e73238832e1
Leonardo <noreply@leoflo.me>
2025-11-16 09:49:51 +0100

    cutter

--- hermes/nixos/programs/cybersec.nix
+++ hermes/nixos/programs/cybersec.nix
@@ -4,7 +4,7 @@
   config = lib.mkIf config.cybersec.enable {
     # Packages
     environment.systemPackages = with pkgs; [
-      ascii file binwalk codeql
+      ascii file binwalk codeql pwntools
       gnat15 gdb gef
       ghidra-bin cutter
       dig wireshark-qt
