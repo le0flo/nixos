0f44b26a3e2a459690b940ac489d70920dd49e47
Leonardo <noreply@leoflo.me>
2025-11-16 10:42:28 +0100

    cutter with plugins

--- hermes/nixos/programs/cybersec.nix
+++ hermes/nixos/programs/cybersec.nix
@@ -1,12 +1,16 @@
-{lib, config, pkgs, ...}: {
+{lib, config, pkgs, ...}:
+  let
+    rizin = pkgs.rizin.withPlugins(ps: with ps; [ jsdec rz-ghidra sigdb ]);
+    cutter = pkgs.cutter.withPlugins(ps: with ps; [ rz-ghidra ]);
+  in {
   options.cybersec.enable = lib.mkEnableOption "cybersecurity tooling";
 
   config = lib.mkIf config.cybersec.enable {
     # Packages
     environment.systemPackages = with pkgs; [
       ascii file binwalk codeql pwntools
-      gnat15 python313 gdb gef
-      ghidra-bin cutter cutterPlugins.rz-ghidra
+      gnat15 python313 gdb gef rizin
+      ghidra-bin cutter
       dig wireshark-qt
       postman burpsuite
     ];
