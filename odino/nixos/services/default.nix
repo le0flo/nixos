c2ff75169d32600f671c3f6ea0da2e268779f0a3
Leonardo <noreply@leoflo.me>
2025-12-26 11:39:18 +0100

    xmrig :D

--- odino/nixos/services/default.nix
+++ odino/nixos/services/default.nix
@@ -9,4 +9,25 @@
       PrintMotd = false;
     };
   };
+
+  # XMRig
+  services.xmrig = {
+    enable = true;
+
+    settings = {
+      autosave = true;
+      cpu = true;
+      opencl = false;
+      cuda = false;
+      pools = [
+        {
+          algo = "rx/0";
+          url = "xmr-eu1.nanopool.org:10343";
+          user = "89erpoKiHuXbw1e1t8UZjNT1AX1Y9z1pta3f6akwG8okZSaLvFQ1eCJ4ih3tSH4Q9BPqHua3vfn6w1ZjQ98TQQHx5GHaRqf";
+          keepalive = true;
+          tls = true;
+        }
+      ];
+    };
+  };
 }
