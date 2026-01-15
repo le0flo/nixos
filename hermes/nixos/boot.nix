aae66010d342ea700a5c0525bdcaa31f2e7d40a8
Leonardo <noreply@leoflo.me>
2026-01-15 16:43:08 +0100

    semplificato il processo di boot

--- hermes/nixos/boot.nix
+++ hermes/nixos/boot.nix
@@ -1,19 +1,8 @@
-{config, ...}: {
+{...}: {
   boot = {
-    consoleLogLevel = 3;
+    consoleLogLevel = 4;
 
-    kernelParams = [
-      "quiet"
-      "splash"
-      "udev.log_priority=3"
-      "boot.shell_on_fail"
-    ];
-
-    extraModulePackages = with config.boot.kernelPackages; [
-      v4l2loopback
-    ];
-
-    extraModprobeConfig = "options v4l2loopback devices=1 video_nr=1 card_label=\"OBS Cam\" exclusive_caps=1";
+    kernelParams = [ "boot.shell_on_fail" ];
 
     loader = {
       timeout = 3;
@@ -22,10 +11,5 @@
     };
 
     supportedFilesystems = [ "exfat" "ntfs" ];
-
-    plymouth = {
-      enable = true;
-      theme = "bgrt";
-    };
   };
 }
