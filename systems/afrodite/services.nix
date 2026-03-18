2d6ff1bf2f038cfb495dae12b83349730175bba5
Leonardo <noreply@leoflo.me>
2026-03-18 18:35:53 +0100

    bind abilitato

--- systems/afrodite/services.nix
+++ systems/afrodite/services.nix
@@ -2,9 +2,11 @@
   imports = [
     ../../components/services/openssh.nix
     ../../components/services/caddy.nix
+    ../../components/services/bind.nix
   ];
 
   # Custom modules
   openssh.enable = true;
   caddy.enable = true;
+  bind.enable = true;
 }
