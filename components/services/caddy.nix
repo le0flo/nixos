78e2296833baee066872711d4a45abcd3626dcdc
Leonardo <noreply@leoflo.me>
2026-03-22 11:03:53 +0100

    caddy aggiornato

--- components/services/caddy.nix
+++ components/services/caddy.nix
@@ -21,6 +21,14 @@
           respond "Benvenuto nella rete privata di leo :D"
           tls internal
         '';
+
+        "music.home.leoflo.me".extraConfig = ''
+          reverse_proxy 10.69.0.2:9001
+        '';
+
+        "cinema.home.leoflo.me".extraConfig = ''
+          reverse_proxy 10.69.0.2:9004
+        '';
       };
     };
   };
