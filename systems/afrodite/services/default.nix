ce429d6df13b101e9c0a48b4f3989cc6537c609d
Leonardo <noreply@leoflo.me>
2026-04-10 20:25:59 +0200

    kubernetes disabilitato (per ora)

--- systems/afrodite/services/default.nix
+++ systems/afrodite/services/default.nix
@@ -1,7 +1,6 @@
 {...}: {
   imports = [
     ../../../components/services/openssh.nix
-    ../../../components/services/k8s/master.nix
 
     ./bind.nix
     ./caddy.nix
--- systems/odino/services.nix
+++ systems/odino/services.nix
@@ -1,6 +1,5 @@
 {...}: {
   imports = [
     ../../components/services/openssh.nix
-    ../../components/services/k8s/node.nix
   ];
 }
