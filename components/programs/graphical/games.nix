c5bca4e50ba71faac6962ac7df120dedc53f26eb
Leonardo <noreply@leoflo.me>
2026-04-21 11:40:18 +0200

    runelite aggiunto

--- components/programs/graphical/games.nix
+++ components/programs/graphical/games.nix
@@ -10,8 +10,14 @@
   environment.systemPackages = with pkgs; [
     prismlauncher
     heroic
+    runelite
 
     gpu-screen-recorder
     gpu-screen-recorder-gtk
   ];
+
+  # Runelite res fix
+  environment.sessionVariables = {
+    _JAVA_AWT_WM_NONREPARENTING = "1";
+  };
 }
