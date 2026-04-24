efa749b1f7409bda8443717a83834cf07f284781
Leonardo <noreply@leoflo.me>
2026-04-24 12:59:42 +0200

    bash prompt fixato

--- components/programs/bash.nix
+++ components/programs/bash.nix
@@ -4,7 +4,7 @@
     vteIntegration = true;
 
     promptInit = ''
-      export PS1='\e[1m\[$(tput setaf 1)\][\[$(tput setaf 3)\]\u\[$(tput setaf 2)\]@\[$(tput setaf 6)\]\h \[$(tput setaf 5)\]\w\[$(tput setaf 1)\]]\e(B\e[m\$ '
+      export PS1='\[$(tput bold)$(tput setaf 1)\][\[$(tput setaf 3)\]\u\[$(tput setaf 2)\]@\[$(tput setaf 6)\]\h \[$(tput setaf 5)\]\w\[$(tput setaf 1)\]]\[$(tput sgr0)\]\$ '
     '';
 
     shellAliases = {
