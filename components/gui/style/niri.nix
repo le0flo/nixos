d4937dc3603e848e06b32fdae75030b61abe02ee
Leonardo <noreply@leoflo.me>
2026-05-11 21:11:29 +0200

    smaller cursor

--- components/gui/style/niri.nix
+++ components/gui/style/niri.nix
@@ -1,11 +1,20 @@
 {config, ...}: {
-  xdg.configFile."niri/colors.kdl".text = ''
-  layout {
-    border {
-      active-color "${config.lib.stylix.colors.base0D}"
-      inactive-color "${config.lib.stylix.colors.base02}"
-      urgent-color "${config.lib.stylix.colors.base08}"
+  xdg.configFile = {
+    "niri/colors.kdl".text = ''
+    layout {
+      border {
+        active-color "${config.lib.stylix.colors.base0D}"
+        inactive-color "${config.lib.stylix.colors.base02}"
+        urgent-color "${config.lib.stylix.colors.base08}"
+      }
     }
-  }
-  '';
+    '';
+
+    "niri/cursor.kdl".text = ''
+    cursor {
+      xcursor-theme "${config.stylix.cursor.name}"
+      xcursor-size ${builtins.toString config.stylix.cursor.size}
+    }
+    '';
+  };
 }
