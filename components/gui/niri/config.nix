70ae9f243d9469de1cb6fc7563e90280a0a38746
Leonardo <noreply@leoflo.me>
2026-05-14 13:50:17 +0200

    inode/directory fix per niri e rimossi gli home.arpa da nginx

--- components/gui/niri/config.nix
+++ components/gui/niri/config.nix
@@ -1,3 +1,11 @@
 {...}: {
-  xdg.configFile."niri/config.kdl".text = builtins.readFile ./config.kdl;
+  xdg = {
+    configFile."niri/config.kdl".text = builtins.readFile ./config.kdl;
+
+    mimeApps = {
+      enable = true;
+
+      defaultApplications."inode/directory" = [ "thunar.desktop" ];
+    };
+  };
 }
--- systems/afrodite/services/nginx.nix
+++ systems/afrodite/services/nginx.nix
@@ -21,38 +21,14 @@ in {
 
       "files.${domainName}" = {
         root = "/srv/files.leoflo.me";
+
         useACMEHost = domainName;
         addSSL = true;
 
         locations."/".extraConfig = ''autoindex on;'';
       };
 
-      # Internal (homenet only)
-      "home.arpa" = {
-        locations."/".return = ''200 "Benvenuto nella rete privata di leo :D"'';
-      };
-
-      "music.home.arpa" = {
-        locations."/".proxyPass = "http://10.69.0.2:9001";
-      };
-
-      "images.home.arpa" = {
-        locations."/".proxyPass = "http://10.69.0.2:9002";
-      };
-
-      "papers.home.arpa" = {
-        locations."/".proxyPass = "http://10.69.0.2:9003";
-      };
-
-      "cinema.home.arpa" = {
-        locations."/".proxyPass = "http://10.69.0.2:9004";
-      };
-
-      "torrent.home.arpa" = {
-        locations."/".proxyPass = "http://10.69.0.2:9005";
-      };
-
-      # Internal (public facing)
+      # Homelab (public facing)
       "music.home.${domainName}" = {
         useACMEHost = domainName;
         forceSSL = true;
