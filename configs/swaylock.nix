d9e5248c4d4d378ca3202ce6e1b0c8b444d086d2
Leonardo <noreply@leoflo.me>
2026-05-27 16:48:13 +0200

    colori di swaylock

--- configs/swaylock.nix
+++ configs/swaylock.nix
@@ -2,6 +2,7 @@
 
 let
   style = import ../gui/style.nix { inherit pkgs; };
+  parseColor = color: builtins.substring 1 6 color;
 in {
   xdg.config.files."swaylock/config".text = ''
     ignore-empty-password
@@ -10,11 +11,39 @@ in {
     indicator-idle-visible
     indicator-radius=100
 
+    line-uses-inside
+
     clock
     timestr=%H:%M:%S
     datestr=%d %B
 
     image=${config.xdg.config.directory}/background/wallpaper
-    color=${style.colors.background}
+    color=${parseColor style.colors.background}
+
+    inside-color=${parseColor style.colors.background}
+    inside-clear-color=${parseColor style.colors.background}
+    inside-caps-lock-color=${parseColor style.colors.background}
+    inside-ver-color=${parseColor style.colors.background}
+    inside-wrong-color=${parseColor style.colors.background}
+
+    key-hl-color=${parseColor style.colors.primary}
+    caps-lock-key-hl-color=${parseColor style.colors.primary}
+
+    bs-hl-color=${parseColor style.colors.secondary}
+    caps-lock-bs-hl-color=${parseColor style.colors.secondary}
+
+    ring-color=${parseColor style.colors.background}
+    ring-clear-color=${parseColor style.colors.background}
+    ring-caps-lock-color=${parseColor style.colors.background}
+    ring-ver-color=${parseColor style.colors.background}
+    ring-wrong-color=${parseColor style.colors.background}
+
+    separator-color=${parseColor style.colors.background}
+
+    text-color=${parseColor style.colors.text}
+    text-clear-color=${parseColor style.colors.text}
+    text-caps-lock-color=${parseColor style.colors.text}
+    text-ver-color=${parseColor style.colors.text}
+    text-wrong-color=${parseColor style.colors.text}
   '';
 }
