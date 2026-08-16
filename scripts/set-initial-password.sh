5396453c6943db4e2cc1d9e2c0439698794cb5a0
Leonardo <noreply@leoflo.me>
2026-08-16 17:05:03 +0200

    scripts/set-initial-password.sh finito

--- scripts/set-initial-password.sh
+++ scripts/set-initial-password.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
-read -s -p "Root password: " ROOT_PASSWORD
+if [ $# == 0 ]; then
+    read -s -p "Root password: " ROOT_PASSWORD
+    echo
 
-HASH=$(mkpasswd -m yescrypt "$ROOT_PASSWORD")
+    if [ -z $ROOT_PASSWORD ]; then
+        echo "Empty passwords aren't allowed" >&2
+        exit 1
+    fi
 
-# TODO: da continuare
+    HASH=$(mkpasswd -m yescrypt "$ROOT_PASSWORD")
+    ESCAPED_HASH=$(printf '%s' "$HASH" | sed 's/[&/\]/\\&/g')
+
+    sed -i.bak "s|\"root\".initialHashedPassword = null;|\"root\".initialHashedPassword = \"$ESCAPED_HASH\";|" hosts/template.nix
+
+    echo "Initial password set"
+elif [ $1 == "reset" ]; then
+    git restore hosts/template.nix
+    rm -f hosts/*.bak
+
+    echo "Initial password reset"
+fi
