b7c2b887b285bc6886df70eca68a4dc00dab75f4
Leonardo <noreply@leoflo.me>
2026-08-20 09:40:23 +0200

    rofi default theme

--- modules/otis/gui/default.nix
+++ modules/otis/gui/default.nix
@@ -52,39 +52,49 @@ in {
 
     otis.hjem = [
       {
-        xdg.config.files."alacritty/alacritty.toml" = {
-          type = "copy";
-          permissions = "644";
+        xdg.config.files = {
+          "alacritty/alacritty.toml" = {
+            type = "copy";
+            permissions = "644";
 
-          generator = (pkgs.formats.toml {}).generate "alacritty.toml";
-          value = {
-            window = {
-              padding = {
-                x = 5;
-                y = 5;
+            generator = (pkgs.formats.toml {}).generate "alacritty.toml";
+            value = {
+              window = {
+                padding = {
+                  x = 5;
+                  y = 5;
+                };
+
+                opacity = 1.0;
+                blur = false;
               };
 
-              opacity = 1.0;
-              blur = false;
-            };
-
-            env = {
-              TERM = "xterm-256color";
-              WINIT_X11_SCALE_FACTOR = "1.0";
-            };
-
-            font = {
-              size = 18.00;
-
-              normal = {
-                family = "ComicShannsMono Nerd Font Mono";
-                style = "Regular";
+              env = {
+                TERM = "xterm-256color";
+                WINIT_X11_SCALE_FACTOR = "1.0";
               };
-              bold = {
-                family = "ComicShannsMono Nerd Font Mono";
-                style = "Bold";
+
+              font = {
+                size = 18.00;
+
+                normal = {
+                  family = "ComicShannsMono Nerd Font Mono";
+                  style = "Regular";
+                };
+                bold = {
+                  family = "ComicShannsMono Nerd Font Mono";
+                  style = "Bold";
+                };
               };
             };
+          };
+          "rofi/config.rasi" = {
+            type = "copy";
+            permissions = "644";
+
+            text = ''
+              @theme "${pkgs.rofi}/share/rofi/themes/Arc-Dark.rasi"
+            '';
           };
         };
       }
