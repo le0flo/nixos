ac06abe8dd151396238a32089bc4f3a60720b956
Leonardo <noreply@leoflo.me>
2026-08-27 22:22:13 +0200

    microvms: +sharing funzionante

--- microvms/sharing/default.nix
+++ microvms/sharing/default.nix
@@ -1,8 +1,12 @@
-{microvm, ...}:
+{microvm, serviceName, ...}:
 
 let
-  btPort = 10005;
-  slskPort = 10006;
+  btPort = 11001;
+  btDownloadDir = "/media/bt";
+
+  slskPort = 11002;
+  slskEnvDir = "/run/secrets/slsk";
+  slskDownloadDir = "/media/slsk";
 in {
   microvm = {
     forwardPorts = [
@@ -17,6 +21,49 @@ in {
         guest.port = slskPort;
       }
     ];
+
+    shares = [
+      {
+        tag = "soulseek-secrets";
+        source = "/run/agenix/microvms/slskd";
+        mountPoint = slskEnvDir;
+        readOnly = true;
+
+        proto = "virtiofs";
+      }
+      {
+        tag = "bittorrent-downloads";
+        source = "/mnt/storage/downloads/bt";
+        mountPoint = btDownloadDir;
+
+        proto = "virtiofs";
+        posixAcl = false;
+        extraArgs = [
+          "--translate-uid" "map:998:1000:1"
+          "--translate-gid" "map:998:100:1"
+        ];
+      }
+      {
+        tag = "soulseek-downloads";
+        source = "/mnt/storage/downloads/slsk";
+        mountPoint = slskDownloadDir;
+
+        proto = "virtiofs";
+        posixAcl = false;
+        extraArgs = [
+          "--translate-uid" "map:997:1000:1"
+          "--translate-gid" "map:997:100:1"
+        ];
+      }
+    ];
+
+    volumes = [
+      {
+        image = "${serviceName}-data.img";
+        mountPoint = "/var/lib";
+        size = 4096;
+      }
+    ];
   };
 
   networking.firewall.allowedTCPPorts = [
@@ -29,12 +76,29 @@ in {
       enable = true;
 
       webuiPort = btPort;
+      serverConfig = {
+        LegalNotice.Accepted = true;
+        Preferences = {
+          Downloads = {
+            SavePath = "${btDownloadDir}/complete";
+            TempPath = "${btDownloadDir}/incomplete";
+          };
+          WebUI.AlternativeUIEnabled = true;
+        };
+      };
     };
 
     slskd = {
       enable = true;
-    
-      settings.web.port = slskPort;
+
+      environmentFile = "${slskEnvDir}/environment";
+      settings = {
+        directories = {
+          downloads = "${slskDownloadDir}/complete";
+          incomplete = "${slskDownloadDir}/incomplete";
+        };
+        web.port = slskPort;
+      };
     };
   };
 }
